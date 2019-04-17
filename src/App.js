7810d4e980910a6e70c9398fd0d08c290d4eebf6
swapnil lothe <44019530+swapnillothe@users.noreply.github.com>
2019-04-17 18:30:40 +0530

    Intialize the todo app with some hard coded todos.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const Todo = function(props) {
+    const { todo } = props;
+    return <div>{todo}</div>;
+};
+
+const TodoList = function(props) {
+    const { todoList } = props;
+    const todoListHtml = todoList.map((todo) => <Todo todo={todo} />);
+    return todoListHtml;
+};
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = { todoList: [ 1, 2 ] };
+    }
+
+    render() {
+        return <TodoList todoList={this.state.todoList} />;
+    }
 }
 
 export default App;
